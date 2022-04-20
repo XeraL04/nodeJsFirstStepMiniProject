bca4a6244f6f62eb64f9e6898f2fadd339fe35dd
Xeral04 <faresbelarbi@outlook.com>
2022-04-20 14:58:23 +0100

    checkpoint

--- Controller/showCollections.controller.js
+++ Controller/showCollections.controller.js
@@ -7,151 +7,82 @@ const client = new MongoClient(process.env.URL);
 const database = client.db("active_tracking");
 
 
+
+
+
 //=====================================================================//
+
+function findCollectionInfo(database){
+    return new Promise(async resolve => {
+        await database.listCollections().toArray(async function(err,collInfos){ 
+                resolve(collInfos)
+        })   
+    })
+}
+
+//=====================================================================//
+
 function needdedCollections (database){ 
     var newTab
-
+    
     return new Promise(async resolve => {
-       
+        
         console.log("from sofian")
-        await database.listCollections().toArray(async function(err,collInfos){    
-            var objTab = [] ; 
-            console.log("1 Starting promise listCollections ") ;
-            newTab = await new Promise(async resolve => {
-                // console.log(collInfos)
+        
+        var objTab = [] ; 
+        
+        collInfos = await findCollectionInfo(database) ;
+        console.log(collInfos.length); 
+        // after we get access to the database and get our collections listed now
+        // we start a new promise and loop inside those collections to get what we want. 
+        newTab = await new Promise(async resolve => {
+            
+            console.log("2 loop collInfos ") ;
+            for ( col of collInfos) { 
                 
-                console.log("2 loop collInfos ") ;
-                 for ( col of collInfos) { 
-                                       
-                    if(col.name != "postdocs"){   
-                        console.log("3 Starting promise collInfos ") ; 
-                        size = await getCollectionCounts(col.name) ;                            console.log(size);
-                        var obj = {      
-                            name :  col.name          ,
-                            count : "" + size
-                        }   
-                        console.log("4 End promise collInfos ") ;                                                    
-                    }                              
-                    objTab.push(obj) 
-                   
+                  
+                    console.log("3 Starting promise collInfos ") ; 
+                    size = await getCollectionCounts(col.name) ;                            
+                    console.log(size);
+                    var obj = {      
+                        name :  col.name          ,
+                        count : "" + size
+                    }   
+                    console.log("4 End promise collInfos ") ;                                                    
+                                            
+                objTab.push(obj) 
+                
                 console.log("5- en loop ") ;  
                 console.log(objTab)                
-               
-                } ;
-                console.log("6- out of the loop ") ;  
-            
-                resolve(objTab);
-            
-            }) ;
-            console.log("6 End promise listcollection ") ;
-        
-             console.log(newTab);
-            
-
-        })     
-        // return newTab ;
-
-
-
-        resolve(newTab);
-    })       
-        
-}
-
-// function needdedCollections (database){ 
-//     var newTab
-
-//     return new Promise(async resolve => {
-       
-//         console.log("from sofian") ;
-        
-//         database.listCollections().toArray(async function(err,collInfos){  })        
-//             var objTab = [] ; 
-//             console.log("1 Starting promise listCollections ") ;
-//             newTab = await new Promise(async resolve => {
-//                 // console.log(collInfos)
                 
-//                 console.log("2 loop collInfos ") ;
-//                  for ( col of collInfos) { 
-                                       
-//                     if(col.name != "postdocs"){   
-//                         console.log("3 Starting promise collInfos ") ; 
-//                         size = await getCollectionCounts(col.name) ;                            
-//                         console.log(size);
-//                         var obj = {      
-//                             name :  col.name          ,
-//                             count : "" + size
-//                         }   
-//                         console.log("4 End promise collInfos ") ;                                                    
-//                     }                              
-//                     objTab.push(obj) 
-                   
-//                 console.log("5- en loop ") ;  
-//                 console.log(objTab)                
-               
-//                 } ;
-//                 console.log("6- out of the loop ") ;  
+            } ;
+            console.log("6- out of the loop ") ;  
             
-//                 resolve(objTab);
+            resolve(objTab);
             
-//             }) ;
-//             console.log("6 End promise listcollection ") ;
+        }) ;
+        console.log("6 End promise listcollection ") ;
         
-//              console.log(newTab);
-            
-
-       
-//         // return newTab ;
-
-
-
-//         resolve(newTab);
-//     })       
+        console.log(newTab);
         
-// }
+        resolve(newTab);
+    })}
 
-// function findCollections (database){    
-//     return new Promise(resolve=>{
-//         const result = needdedCollections(database);
-//         console.log("7 ") ;
-//         console.log(result);
-//         resolve (result)
-//     })
-   
-// }
-
-
-async function findCollections (database){
-
-    const result = await needdedCollections(database);
-    console.log("7") ;
-    // console.log(result);
-    console.log(result);
-    // return result
-}
-
-
-
-
+//==========================getCollectionEndPoint===========================================//
 
 collectionController.getcollections = async (req,res)=>{
-
+    
     try{
-        await client.connect();
 
-        cls = await needdedCollections(database);
+        await client.connect();        
+        collex = await needdedCollections(database);
         console.log("7 ........ ") ;
-            // resolve(cls);
-               
-        console.log(collex);
-
         res.send(collex);
+
     }catch(error){
         res.status(500).send(error)
     }
-    
 }
-
 
 //==============================getCollectionCounts=======================================//
 
@@ -176,136 +107,158 @@ collectionController.getcollections = async (req,res)=>{
     });
 }
 
-//===============================getCollectionCountsEndPoint======================================//
-
-collectionController.numberOfDocuments = async (req,res)=>{
-    try {
-        await client.connect();
-        collex = await findCollections(database);
-        
-        tab = [];
-
-        collex.forEach(async (col) => {
-            t =  await getCollectionCounts(col.name);
-            tab.push(t);
-        });
-
-        res.send(tab)
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
-    }
-}
-
-
-
-
-
-
-
-
-
 //===============================IMEIstart======================================//
 
-function getTheImei (collection){
+
+
+function getTheImei (database){
+    var theFinalTab
     return new Promise (async resolve=> {
-        const result = collection.aggregate([{$group:{_id:"$imei",count:{$sum:1}}}])
-        let setObject =[] ; 
 
-        try {
-            for await (const doc of result){
-                console.log(`what happening here`);
-                if (doc._id !=null){
-                    objct = {
-                        imei : doc._id,
-                        count : doc.count
-                    }
-                    setObject.push(objct)
-            }}
-            console.log(`it's done`);
+        var objectTab =[];
 
-                resolve (setObject)
-        } catch (error) {
-            resolve(error)
-        }
+        callCollImei = await findCollectionInfo2(database);
+
+        console.log(`the access to the db is OK and you have ${callCollImei.length} collections`);
+
+        theFinalTab = await new Promise (async resolve => {
+
+            for (coli of callCollImei){
+                console.log(`aggregate of collections`);
+                console.log(coli.name);
+                console.log(`inside the db and startinga new loop`);
+
+                const actualCollection = client.db("active_tracking").collection(coli.name);
+                imeiListCursor = actualCollection.aggregate([{$group:{_id:"$imei",count:{$sum:1}}}]);
+                console.log(imeiListCursor);
+
+                var imeiTab = [];
+
+                for await (const doc of imeiListCursor){
+                    console.log(doc);
+
+                    imeiTab.push(doc)
+                }
+
+                var obj = { 
+                    name : coli.name, 
+                    imeiList : imeiTab
+                }
+
+                objectTab.push(obj)
+            } 
+        console.log(objectTab);
+        resolve(objectTab)
+        })
+        resolve (theFinalTab)
     })}
-//=====================================================================//
 
-async function findImei (collection){
-    const result = await getTheImei(collection);
-    console.log(result);
-    console.log(`2.find Imei`);
-    return result
-}
+   
+
+//===============================================================================//
+
+// async function findImei (collection){
+//     const result = await getTheImei(collection);
+//     console.log(result);
+//     // console.log(`2.find Imei`);
+//     return result
+// }
 
 
-const collection = client.db("active_tracking").collection("data_3546365045555737401")
+// const collection = client.db("active_tracking").collection("data_3546365045555737401")
+
 //==============================ImeiEndPoint=======================================//
 
 collectionController.findTheImei = async(req,res)=>{
     try {
         await client.connect();
-        console.log(`1.access to the db`);
-        allImei = await findImei(collection);
+   
+        // console.log(`1.access to the db`);
+   
+        allImei = await getTheImei(database);
         
-        // tabl = []
-        // console.log(`3.using the eachfor`);
-
-        // allImei.forEach(async (collection) => {
-        //     k = await getTheImei(collection);
-        //     tabl.push(k)
-        // })
-        // console.log(tabl);
-        console.log(`3.the end`);
+   
+        // console.log(`3.the end`);
+   
         res.send(allImei)
+   
     } catch (error) {
+   
         console.log(error);
+   
         res.status(500).send(error)
+   
     }
 }
+ 
+//==================================listVehicleStart===================================//
 
-// the question is : how to made the function avalibal to all the collections.
-
-
-
-
-
-
-
-
-//==================================listVehicle===================================//
-
-function getTheVehicleList (collection){
+function getTheVehicleList (database){
+    
+    var nowTab 
     return new Promise (async resolve=> {
-        const result = collection.aggregate([{$group:{_id:"$deviceLabel",count:{$sum:1}}}])
-        let setObject =[] ; 
 
-        try {
-            for await (const doc of result){
-                console.log(`what happening here`);
-                if (doc._id !=null){
-                    objct = {
-                        deviceLabel : doc._id,
-                        count : doc.count
-                    }
-                    setObject.push(objct)
-            }}
-            console.log(`it's done`);
+        var objctTab = [];
 
-                resolve (setObject)
-        } catch (error) {
-            resolve(error)
-        }
+        callInfosV = await findCollectionInfo2(database)
+
+    
+
+        console.log(`the access to the db is OK and you have ${callInfosV.length} collections`);
+        
+        nowTab = await new Promise(async resolve =>{
+
+            for (coil of callInfosV){
+               // console.log(`qgrregqte of collection `);
+               // console.log(coil.name); 
+               // console.log(`inside the bd and starting a new loop`);
+               const currentCollection = client.db("active_tracking").collection(coil.name)
+               vehicleListCursor = currentCollection.aggregate([{$group:{_id:"$deviceLabel",count:{$sum:1}}}])
+                // console.log(vehicleListCursor);
+                var vehicleTab = [];
+
+
+                for await (const doc of vehicleListCursor) {
+                    
+                //console.log(doc);
+                    vehicleTab.push(doc)
+                }
+
+                var obj = {
+                    name : coil.name,
+                    vehicleList : vehicleTab
+                }
+               // console.log(`========== wowowo we are here ==========`);
+                objctTab.push(obj)
+
+            }
+            console.log(objctTab);
+            resolve(objctTab)
+        });
+        
+        resolve (nowTab)
     })}
 
-//=====================================================================//
+//========================================================================================//
 
-async function findVehicleList (collection){
-    const result = await getTheVehicleList(collection);
-    console.log(result);
-    console.log(`2.find vehicle list`);
-    return result
+//  function getTheViListDis (database) {
+//     return new Promise((async resolve=>{
+//         await database.collection.distinct("deviceLabel").then ((vList)=>{console.log(vList)
+//         }).catch((err)=>{
+//             console.log(err.message);
+//         })
+//         resolve(vList)
+//     })
+// )}
+
+//========================================================================================//
+
+function findCollectionInfo2(database){
+    return new Promise(async resolve => {
+        await database.listCollections().toArray(async function(err,collInfos){ 
+            resolve(collInfos)
+        })   
+    })
 }
 
 //=================================vehicleListEndPoint====================================//
@@ -315,13 +268,13 @@ collectionController.findTheVehicleList = async(req,res)=>{
     
         await client.connect();
     
-        console.log(`1.access to the db`);
-    
-        allImei = await findVehicleList(collection);
+        // console.log(`1.access to the db`);
+
+        allvehicles = await getTheVehicleList(database);
         
-        console.log(`3.the end`);
+        // console.log(`5.the end`);
         
-        res.send(allImei)
+        res.send(allvehicles)
     
     } catch (error) {
     
@@ -331,6 +284,6 @@ collectionController.findTheVehicleList = async(req,res)=>{
     
     }
 }
-//=====================================================================//
+//=========================================================================================//
 
 module.exports = collectionController;
--- Router/showCollections.router.js
+++ Router/showCollections.router.js
@@ -7,7 +7,7 @@ const collectionController = require('../Controller/showCollections.controller.j
 
 //router:
 router.get("/collection", collectionController.getcollections)
-router.get("/countDocuments", collectionController.numberOfDocuments)
+// router.get("/countDocuments", collectionController.numberOfDocuments)
 router.get("/getImei", collectionController.findTheImei)
 router.get("/vehicleList", collectionController.findTheVehicleList)
 // router.get("/listImei",c)
